17f943d1f5cbb68ea0ee16db9c689336e6990070
Mjones13 <jonesmal@umich.edu>
2018-09-12 22:42:32 -0700

    Created index to render to DOM

---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PledgeList from './PledgeList.jsx';
+
+
+// console.log('this is what pledge list looks like', <PledgeList />);
+ReactDOM.render((<PledgeList />), document.getElementById('support'));
