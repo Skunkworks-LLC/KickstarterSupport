911d515d3f3e35f6566da100eda3967ec0192ca4
Mjones13 <jonesmal@umich.edu>
2018-09-13 14:12:09 -0700

    Forms connection with Mongodb database

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/kickstarter').then(() => {
+  console.log('Connected to the database at port -> 27017');
+});
+
+const db = mongoose.connection;
+
+db.on('error', err => console.error(err));
+
+module.exports = db;
