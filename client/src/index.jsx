d390a9fbbb711029e0ba71856cbeeb24be0064c4
Mjones13 <jonesmal@umich.edu>
2018-09-13 14:14:35 -0700

    Removed comments

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PledgeList from './PledgeList';
 
 
-// console.log('this is what pledge list looks like', <PledgeList />);
 ReactDOM.render((<PledgeList />), document.getElementById('support'));
