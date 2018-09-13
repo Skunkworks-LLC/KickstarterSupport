585f6fa5d2d2f2fc981129f645effb56248e0567
Mjones13 <jonesmal@umich.edu>
2018-09-13 15:38:32 -0700

    Created seperate component for pledge availability

---
+++ client/src/Availability.jsx
@@ -0,0 +1,29 @@
+/* global React */
+
+class Availability extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { availability, numBackers } = this.props;
+    this.state = {
+      isPledgeAvailable: availability,
+      numBackers,
+    };
+  }
+
+  render() {
+    const { isPledgeAvailable, numBackers } = this.state;
+    const Status = isPledgeAvailable
+      ? (
+        <div className="pledgeSubheaderFont" id="pledgeAvailable">
+          {numBackers}
+          <span> backers</span>
+        </div>
+      )
+      : <div className="pledgeTextFont" id="pledgeNotAvailable">Reward no longer Available</div>;
+
+    return Status;
+  }
+}
+
+export default Availability;
