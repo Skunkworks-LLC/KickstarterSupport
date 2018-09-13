e8689cc34a8f6efb73ad5e6cdac92a27a1af99e6
Mjones13 <jonesmal@umich.edu>
2018-09-12 22:41:02 -0700

    Created basic Pledge component

---
+++ client/src/Pledge.jsx
@@ -0,0 +1,22 @@
+class Pledge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      available: true,
+      minimumPledgeAmount: 0,
+      pledgeTitle: '',
+      pledgeDescription: '',
+      pledgeRewards: [],
+      estimatedShipping: '',
+      validLocations: [],
+      backers: 0,
+    };
+  }
+
+  render() {
+    // needs to hold all of the data that's in the data structure sheet
+    return (<div>An individual pledge</div>);
+  }
+}
+
+export default Pledge;
