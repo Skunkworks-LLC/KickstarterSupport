3861dbf6a24e8e9a72bf868896a0b2fc037215a9
Mjones13 <jonesmal@umich.edu>
2018-09-15 17:01:38 -0700

    Moved hover functionality to css

--- client/src/Pledge.jsx
+++ client/src/Pledge.jsx
@@ -1,55 +1,44 @@
-/* global React */
+import React from 'react';
 import Availability from './Availability';
+import moment from 'moment';
 
 
-class Pledge extends React.Component {
-  constructor(props) {
-    super(props);
 
-    // const { pledgeInfo } = this.props;
-    this.state = {
-      // isPledgeAvailable: pledgeInfo.available,
-      // numBackers: pledgeInfo.pledgeBackers,
-    };
-  }
+const Pledge = ({ pledgeInfo }) => {
+  const {
+    minimumPledgeAmount, pledgeTitle, pledgeDescription, pledgeRewards,
+    estimatedShipping, validLocations, available, pledgeBackers,
+  } = pledgeInfo;
 
-  render() {
-    const { pledgeInfo } = this.props;
-    const {
-      minimumPledgeAmount, pledgeTitle, pledgeDescription, pledgeRewards,
-      estimatedShipping, validLocations, available, pledgeBackers,
-    } = pledgeInfo;
-
-    // const { isPledgeAvailable, numBackers } = this.state;
-
-    return (
-      <div className="pledgeContainer">
-        An individual pledge
-        <div className="pledgeHeaderFont pledgeAmount">
-          <span>Pledge $</span>
-          {minimumPledgeAmount}
-          <span> or more</span>
-        </div>
-        <div className="pledgeHeaderFont pledgeTitle">{pledgeTitle}</div>
-        <div className="pledgeSubheaderFont pledgeDescription">{pledgeDescription}</div>
-        <ul className="pledgeSubheaderFont pledgeRewards">
-          Includes
-          {pledgeRewards.map((reward, index) => <li key={index}>{reward}</li>)}
-        </ul>
-        <div className="pledgeSubheaderFont shippingInfo" id="estimatedShipping">
-          <div>ESTIMATED DELIVERY</div>
-          <div>{estimatedShipping}</div>
-        </div>
-        <div className="shippingInfo" id="validLocations">
-          <div className="pledgeSubheaderFont">SHIPS TO</div>
-          <div className="pledgeTextFont">{validLocations}</div>
-        </div>
-        <Availability availability={available} numBackers={pledgeBackers} />
-        <br />
-        <br />
+  return (
+    <div className="pledgeBoxContainer extended">
+      <div className="pledgeBoxHeaderFont pledgeAmount">
+        <span>Pledge $</span>
+        {minimumPledgeAmount}
+        <span> or more</span>
       </div>
-    );
-  }
-}
+      <div className="pledgeBoxHeaderFont pledgeTitle">{pledgeTitle}</div>
+      <div className="pledgeSubheaderFont pledgeDescription">{pledgeDescription}</div>
+      <ul className="pledgeSubheaderFont pledgeRewards">
+        Includes
+        {pledgeRewards.map((reward, index) => <li key={index}>{reward}</li>)}
+      </ul>
+      <div className="pledgeSubheaderFont shippingInfo" id="estimatedShipping">
+        <div>ESTIMATED DELIVERY</div>
+
+        {/* Format the estimated delivery date using moments */}
+
+        <div>{estimatedShipping}</div>
+      </div>
+      <div className="shippingInfo" id="validLocations">
+        <div className="pledgeSubheaderFont">SHIPS TO</div>
+        <div className="pledgeTextFont">{validLocations}</div>
+      </div>
+      <Availability availability={available} numBackers={pledgeBackers} />
+      <br />
+      <br />
+    </div>
+  );
+};
 
 export default Pledge;
