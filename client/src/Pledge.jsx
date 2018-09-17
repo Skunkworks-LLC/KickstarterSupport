440b849c12b9e62556be315e2b5472bc905f3b49
Mjones13 <jonesmal@umich.edu>
2018-09-16 23:35:47 -0700

    Modularized Shopping Info and Re-structured flexbox

--- client/src/Pledge.jsx
+++ client/src/Pledge.jsx
@@ -1,44 +1,76 @@
 import React from 'react';
 import Availability from './Availability';
 import moment from 'moment';
+import ShippingInfo from './ShippingInfo';
+import InputBox from './InputBox';
+import ContinueButton from './ContinueButton';
+
+
+class Pledge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customerInputBoxes: false,
+      pledgeBoxClasses: "actualBox pledgeBoxContainer"
+    };
+  }
+
+  addCustomerInputBoxes() {
+    this.setState((prevState) => {
+      return (
+        {
+          customerInputBoxes: true,
+          pledgeBoxClasses: 'permanentBorder',
+        });
+    });
+  }
 
 
 
-const Pledge = ({ pledgeInfo }) => {
-  const {
-    minimumPledgeAmount, pledgeTitle, pledgeDescription, pledgeRewards,
-    estimatedShipping, validLocations, available, pledgeBackers,
-  } = pledgeInfo;
 
-  return (
-    <div className="pledgeBoxContainer extended">
-      <div className="pledgeBoxHeaderFont pledgeAmount">
-        <span>Pledge $</span>
-        {minimumPledgeAmount}
-        <span> or more</span>
+  render() {
+    const { pledgeInfo } = this.props;
+    const {
+      minimumPledgeAmount, pledgeTitle, pledgeDescription, pledgeRewards,
+      estimatedShipping, validLocations, available, pledgeBackers,
+    } = pledgeInfo;
+    const { pledgeBoxClasses } = this.state;
+
+    const addCustomerInputBoxes = this.addCustomerInputBoxes.bind(this);
+    return (
+      <div className="outsideBox">
+        {available
+          ? <div></div>
+          : <div id="allGone">All gone!</div>
+        }
+        <div className={pledgeBoxClasses} onClick={() => { addCustomerInputBoxes() }}>
+          <div className="pledgeBoxHeaderFont pledgeAmount pledgeBoxComponentSizing">
+            <span>Pledge US$</span>
+            {minimumPledgeAmount}
+            <span> or more</span>
+          </div>
+          <div className="pledgeBoxHeaderFont pledgeTitle pledgeBoxComponentSizing">{pledgeTitle}</div>
+          <div className="pledgeSubheaderFont pledgeBoxComponentTextSizing" id="pledgeDescription">{pledgeDescription}</div>
+          <ul className="pledgeSubheaderFont pledgeBoxComponentSizing" id="pledgeRewards">
+            Includes:
+              {pledgeRewards.map((reward, index) => <li key={index} className="listItem">{reward}</li>)}
+          </ul>
+          <ShippingInfo estimatedShipping={estimatedShipping} validLocations={validLocations} />
+          <Availability availability={available} numBackers={pledgeBackers} />
+          {
+            this.state.customerInputBoxes
+              ? (
+                <div className="inputBoxesContainer">
+                  <InputBox />
+                  <ContinueButton />
+                </div>
+              )
+              : <div />
+          }
+        </div>
       </div>
-      <div className="pledgeBoxHeaderFont pledgeTitle">{pledgeTitle}</div>
-      <div className="pledgeSubheaderFont pledgeDescription">{pledgeDescription}</div>
-      <ul className="pledgeSubheaderFont pledgeRewards">
-        Includes
-        {pledgeRewards.map((reward, index) => <li key={index}>{reward}</li>)}
-      </ul>
-      <div className="pledgeSubheaderFont shippingInfo" id="estimatedShipping">
-        <div>ESTIMATED DELIVERY</div>
-
-        {/* Format the estimated delivery date using moments */}
-
-        <div>{estimatedShipping}</div>
-      </div>
-      <div className="shippingInfo" id="validLocations">
-        <div className="pledgeSubheaderFont">SHIPS TO</div>
-        <div className="pledgeTextFont">{validLocations}</div>
-      </div>
-      <Availability availability={available} numBackers={pledgeBackers} />
-      <br />
-      <br />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Pledge;
