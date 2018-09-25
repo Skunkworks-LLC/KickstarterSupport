ead0defd109f86453f71ac19712d90e72bc97f01
Mjones13 <jonesmal@umich.edu>
2018-09-24 21:11:29 -0700

    Converted Shpledge input container to styled component

---
+++ client/src/styles/styledPledgeInput.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import { StyledPledgeBox } from './styledPledgeBox';
+
+const StyledInputBox = styled(StyledPledgeBox)`
+  border: '1.5px solid rgba(211, 211, 211, 0.5)';
+`;
+
+export { StyledInputBox };
