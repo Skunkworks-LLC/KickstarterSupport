9cbff404acde483536e0fd1bc4be3b675cafbebb
Mjones13 <jonesmal@umich.edu>
2018-09-24 21:08:50 -0700

    Reduced margins

--- client/src/styles/styledPledgeBox.js
+++ client/src/styles/styledPledgeBox.js
@@ -12,15 +12,17 @@ const StyledPledgeListBox = styled.div`
   }
 `;
 
+
 const StyledPledgeBox = styled.div`
+  z-index: 1;
+
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  align-content: space-around;
   justify-content: center;
 
   margin-top: 3px;
-  padding-top: 10px;
-  padding-bottom: 5px;
   /* border: 1.5px solid rgba(211,211,211, 0.5); */
   border: ${props => props.permanentBorder || !props.available
     ? '1.5px solid rgba(0, 0, 0, 0.5)' : '1.5px solid rgba(211, 211, 211, 0.5)'};
@@ -32,25 +34,129 @@ const StyledPledgeBox = styled.div`
   :hover {
     border: 1.5px solid rgba(0, 0, 0, 0.5);
   }
+
+  .headerFont {
+    font-size: 18px;
+  }
+
+  .subHeaderFont {
+    font-size: 14px;
+    color: rgba(160,160,160, 1.5);
+  }
 `;
 
+const StyledGreenPledgeBox = styled.div`
+  visibility: 'hidden';
+  position: relative;
+  height:100%;
+  width: 100%;
+  font-size: 16px;
+  /* top: 50%; */
+  /* transform: translateY(-50%);  */
 
-const StyledPledgeBoxComponent = styled.div`
+  & + inFront {
+    z-index: 5;
+  }
+
+  .inFront {
+    z-index: 5;
+  }
+
+
+  padding-top: 10px;
+  padding-bottom: 5px;
+  left: -.25px;
+
+  .text {
+    /* height: 100%; */
+    visibility: hidden;
+    /* font-size: 16px; */
+    color: 'white';
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%); 
+  }
+
+  :hover {
+    visibility: ${props => props.available ? 'visible' : 'hidden'};
+    background-color: ${props => props.customerInputBoxes ? 'none' : 'rgba(0, 158, 116, .8)'};
+  }
+  :hover .text {
+    visibility: visible;
+    line-height: 100%;
+    text-align: center;
+    vertical-align: middle;
+    color: white;
+  }
+`;
+
+const StyledLeftSpan = styled.span`
+  justify-content: flex-start;
+  align-content: flex-start;
+  margin-left: 7.5%;
+  margin-right: 7.5%;
+`;
+
+const StyledBoxComponent = styled.div`
   height: auto;
-  flex-basis: 80%;
+  flex-basis: 100%;
   font-size: 14px;
   vertical-align: middle;
   margin-top: 15px;
+
+  .left {
+    margin-left: 7.5%;
+    margin-right: 7.5%;
+  }
 `;
 
-const StyledPledgeBoxComponentText = styled(StyledPledgeBoxComponent)`
+const StyledBoxComponentText = styled(StyledBoxComponent)`
   line-height: 20px;
   margin-top: 10px;
+  margin-left: 7.5%;
+  margin-right: 7.5%;
+`;
+
+const StyledUnorderedList = styled(StyledBoxComponent)`
+  line-height: auto;
+  padding: 0px 0px 0px 0px;
+  margin-left: 7.5%;
+`;
+
+const StyledListItem = styled.li`
+  color: black;
+  line-height: 20px;
+  flex-basis: 50%;
+  font-size: 16px;
+  font-weight: 400;
+  vertical-align: middle;
+  position: relative;
+  right: -10%;
+  top: 1px;
+`;
+
+const StyledSelect = styled.select`
+  height: 40px;
+  font-size: 14px;
+  flex-basis: 80%;
+  vertical-align: middle;
+  border-radius: 0px;
+  border: 1.5px solid rgba(211,211,211, 0.5);
+  margin-top: 5px;
+
+  :focus {
+    outline: none;
+  }
 `;
 
 export {
   StyledPledgeListBox,
   StyledPledgeBox,
-  StyledPledgeBoxComponent,
-  StyledPledgeBoxComponentText,
+  StyledLeftSpan,
+  StyledBoxComponent,
+  StyledBoxComponentText,
+  StyledUnorderedList,
+  StyledListItem,
+  StyledSelect,
+  StyledGreenPledgeBox,
 };
